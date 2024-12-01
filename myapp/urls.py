0f781bc34fd186adee0c144216fb68f42015a1fa
Nabil Majumdar <nabilmajumdar@hotmail.com>
2024-12-01 18:36:11 -0500

    Commit changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('scenario2/', views.scenario2, name='scenario2'),
     path('article1/', views.article1, name='article1'),
     path('logout/', views.logout_view, name='logout'),
-    path('users/', views.user_list, name='user_list'),  
-
+    path('user_list/', views.user_list, name='userlist'),
 ]
